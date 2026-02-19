8a92283ca06e30aed245badbd9423cc52911247d
Lenyx-code <lenyxdev@gmail.com>
2026-02-19 23:07:55 +0100

    update master branch

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PWD}@cl
 mongoose
   .connect(uri)
   .then(() => {
-    app.listen(3000, () => console.log("Server is running on http://localhost:3000/graphql"));
+    const PORT = process.env.PORT;
+    const URL = process.env.URL;
+    app.listen(PORT, () => console.log(`Server is running on ${URL}:${PORT}/graphql`));
   })
   .catch((error) => {
     console.log(error);
